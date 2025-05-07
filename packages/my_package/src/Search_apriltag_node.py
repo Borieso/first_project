4e33551f6595a5e7331968b9870cd3d4cd84469f
Biblo <ureel.bram@gmail.com>
2025-05-07 09:26:37 +0200

    Code previous week

--- packages/my_package/src/Search_apriltag_node.py
+++ packages/my_package/src/Search_apriltag_node.py
@@ -7,11 +7,12 @@ from random import uniform
 from random import randint
 from std_msgs.msg import Bool  # Import Bool message type
 from duckietown.dtros import DTROS, NodeType
-from duckietown_msgs.msg import Twist2DStamped, AprilTagDetectionArray, AprilTagDetection
+from duckietown_msgs.msg import Twist2DStamped, AprilTagDetectionArray, AprilTagDetection, LEDPattern
 from geometry_msgs.msg import Transform, Vector3, Quaternion
 from nav_msgs.msg import Odometry
 from tf import transformations as tr
 from geometry_msgs.msg import Quaternion, Twist, Pose, Point, Vector3, TransformStamped, Transform
+from duckietown_msgs.srv import SetCustomLEDPattern
 
 
 
@@ -35,22 +36,33 @@ class SearchApriltagNode(DTROS):
         # form the message
         self._v = VELOCITY
         self._omega = OMEGA
+
         # construct publisher
         self._publisher = rospy.Publisher(twist_topic, Twist2DStamped, queue_size=1)
         self._reset_odom_publisher = rospy.Publisher(self.reset_odom_topic, Odometry, queue_size=1)
+
         # construct subscriber
         self.object_topic = f"/{self._vehicle_name}/obstacle_detected"
         self.duck_topic = f"/{self._vehicle_name}/duck_detected"
         self.apriltag_topic = f"/{self._vehicle_name}/apriltag_detector_node/detections"
-        self._subscriber_object = rospy.Subscriber(self.object_topic, Bool, self.listen)
-        self._subscriber_duck = rospy.Subscriber(self.duck_topic, Bool, self.listen2)
-        self._subscriber_tag = rospy.Subscriber(self.apriltag_topic, AprilTagDetectionArray  , self.listen3)
+        self._subscriber_object = rospy.Subscriber(self.object_topic, Bool, self.listen_object)
+        self._subscriber_duck = rospy.Subscriber(self.duck_topic, Bool, self.listen_duck)
+        self._subscriber_tag = rospy.Subscriber(self.apriltag_topic, AprilTagDetectionArray  , self.listen_tag)
         self.object_detected = False
         self.duck_detected = False
         self.tag_info = [False, 0, 0] # (, side of the robot , front of the robot)
         self.phase_start_time = rospy.get_time()
 
-        self.x, self.y, self.z = 0
+        # Services
+        self.led_service = rospy.ServiceProxy(f"/{self._vehicle_name}/led_emitter_node/set_custom_pattern", SetCustomLEDPattern)
+        self.led_pattern = LEDPattern()
+        self.led_pattern.color_list = [0,0,0,0,0]
+        self.led_pattern.color_mask = [1, 1, 1, 1, 1]
+        self.led_pattern.frequency = 1
+        self.led_pattern.frequency_mask = [0,0,0,0,0]
+
+
+        self.x, self.y, self.z = 0 , 0 , 0
         self.yaw = 0.0
         self.q = [0.0, 0.0, 0.0, 1.0]
         self.tv = 0.0
@@ -127,9 +139,9 @@ class SearchApriltagNode(DTROS):
         elif(front>0.1):
             rospy.loginfo("front")
             self.drive_forward()
-        else:
-            rospy.loginfo("Draaaiiiii")
-            self.turn_left()
+        # else:
+        #     rospy.loginfo("Draaaiiiii")
+        #     self.turn_left()
 
 
     def run(self):
@@ -141,12 +153,17 @@ class SearchApriltagNode(DTROS):
             if(self.tag_info[0]):
                 self.goto_apriltag(self.tag_info)
                 self.phase_start_time = time
-            elif(self.object_detected):
-                self.drive_backward()
+                #Change leds
+                self.change_led_pattern(['blue', 'blue', 'blue', 'blue', 'blue'], [1, 1, 1, 1, 1], 1)
+
+            # elif(self.object_detected):
+            #     self.change_led_pattern(['green', 'red', 'yellow', 'red', 'green'], [0, 1, 0, 1, 0], 2.5)
+            #     self.drive_backward()
             
-            elif(time-self.phase_start_time>2):
-                rospy.loginfo("Start random walk")
-                self.random_walk(rate)
+            # elif(time-self.phase_start_time>2):
+            #     rospy.loginfo("Start random walk")
+            #     self.change_led_pattern(['yellow', 'yellow', 'yellow', 'yellow', 'yellow'], [0, 0, 0, 0, 0], 1)
+            #     self.random_walk(rate)
             else:
                 self.stop()         
             rate.sleep()
@@ -155,9 +172,9 @@ class SearchApriltagNode(DTROS):
     def reset_odometry(self):
         odom = Odometry()
         odom.header.stamp = rospy.Time.now()  # Ideally, should be encoder time
-        odom.header.frame_id = self.origin_frame
+        #odom.header.frame_id = self.origin_frame
         odom.pose.pose = Pose(Point(self.x, self.y, self.z), Quaternion(*self.q))
-        odom.child_frame_id = self.target_frame
+        #odom.child_frame_id = self.target_frame
         odom.twist.twist = Twist(Vector3(self.tv, 0.0, 0.0), Vector3(0.0, 0.0, self.rv))
 
         self._reset_odom_publisher.publish(odom)
@@ -166,15 +183,33 @@ class SearchApriltagNode(DTROS):
         stop = Twist2DStamped(v=0.0, omega=0.0)
         self._publisher.publish(stop)
         rospy.loginfo("node stopped")
+
+    def change_led_pattern(self, color_list, frequency_mask, freq):
+
+        #Brakke test om te checken of het nodig is om te updaten
+        if(color_list == self.led_pattern.color_list and freq == self.led_pattern.frequency):
+            return
+        
+        self.led_pattern.color_list = color_list # Front L, BAck R, ----, Back L, front R
+        self.led_pattern.color_mask = [1, 1, 1, 1, 1]
+        self.led_pattern.frequency = freq
+        self.led_pattern.frequency_mask = frequency_mask
+
+        
+        try:
+            self.led_service(self.led_pattern)
+            rospy.loginfo("Custom LED pattern set successfully.")
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Service call failed: {e}")
     
-    def listen(self, data):
+    def listen_object(self, data):
         self.object_detected = data.data
         #rospy.loginfo("Object detected: {%s}", data.data)
-    def listen2(self, data):
+    def listen_duck(self, data):
         self.duck_detected = data.data
         if(self.duck_detected):
             rospy.loginfo("Duck is detected!!!")
-    def listen3(self, data : AprilTagDetectionArray):
+    def listen_tag(self, data : AprilTagDetectionArray):
         count = 0
         for detection in data.detections:
             count+=1
